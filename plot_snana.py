0b66746ecc6f7486376969b6ab63e37dc211939e
Justin Roberts-Pierel <jr23@email.sc.edu>
2019-07-10 16:57:03 -0400

    small change

--- plot_snana.py
+++ plot_snana.py
@@ -252,7 +252,7 @@ def main():
 	parser.add_option("--nogrid",help="Do add a grid to the plots.",action="store_true",dest="noGrid",default=False)
 	#parser.add_option("--help",action="store_true",dest='help',default=False)
 	(options,args)=parser.parse_args()
-	
+	print(args)
 	if options.CID=="None":
 		raise RuntimeError("Need to define CID")
 	if options.version is None:
